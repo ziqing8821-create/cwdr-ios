f16258d00af6c7a883bbf1c1101f9131a45c6902
ziqing8821-create <ziqing.8821@gmail.com>
2026-09-17 09:58:02 +0800

    V10.17 sw→cwdr-v117

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 单词萝卜 iOS PWA · 离线缓存：游戏是单文件自包含（词库/精灵全内嵌），缓存一个页面即可离线玩 */
-const CACHE = 'cwdr-v116-' + '1';
+const CACHE = 'cwdr-v117-' + '1';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './apple-touch-icon.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
